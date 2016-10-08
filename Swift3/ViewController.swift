0d0cc13c36bd87d9267b73803683c17eb95aa55e
Hee Hock Sui <heehocksui@hees-macbook-pro.local>
2016-10-08 12:07:13 +0800

    Initial Commit

---
+++ Swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift3
+//
+//  Created by Hee Hock Sui on 08/10/2016.
+//  Copyright © 2016 Hee Hock Sui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
